a4f6e35972e8d03d012e893ad5ea4a194d67d395
Andy Wright <atwright147@gmail.com>
2025-07-22 17:26:46 +0100

    add addGem() test

---
+++ src/utils/addGem.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+
+import type { TokensWithGold } from '../stores/game.store';
+import { addGem } from './addGem';
+
+describe('addGem()', () => {
+  it('should add a gem to the specified color', () => {
+    const gems: TokensWithGold = {
+      red: 0,
+      green: 0,
+      blue: 0,
+      white: 0,
+      black: 0,
+      gold: 0,
+    };
+
+    const result = addGem(gems, 'red');
+
+    expect(result).toEqual({
+      red: 1,
+      green: 0,
+      blue: 0,
+      white: 0,
+      black: 0,
+      gold: 0,
+    });
+  });
+});
